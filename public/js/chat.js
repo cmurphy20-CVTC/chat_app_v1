a2c6b25503a97604da7cdf48d9ca2b597bc8e86f
Casey <89794911+cmurphy20-CVTC@users.noreply.github.com>
2022-06-20 22:57:42 -0500

    users can share their location and profanity filter has been set up

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,10 @@
 
 const socket = io();
 
+// server (emit) -> client (receive) --acknowledgement --> server
+
+// client (emit) -> server (receive) --acknowledgement --> client
+
 socket.on('message', (message) => {
   console.log(message);
 })
@@ -8,18 +12,31 @@ socket.on('message', (message) => {
 
 document.querySelector('#messageForm').addEventListener("submit", (e) => {
 
-e.preventDefault();
+  e.preventDefault();
 
-const message = e.target.elements.message.value;
+  const message = e.target.elements.message.value;
 
-socket.emit('sendMessage', message);
+  socket.emit('sendMessage', message, (error) => {
+    if (error) {
+      return console.log(error)
+    } else {
+      console.log("Message delivered");
+    }
+  });
 })
 
-// socket.on('countUpdated', (count) => {
-//   console.log("The count has been updated!", count)
-// })
+document.querySelector('#sendLocation').addEventListener('click', () => {
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by your browser.')
+  }
 
-// document.querySelector("#increment").addEventListener("click", () => {
-//   console.log("Clicked")
-//   socket.emit('increment')
-// })
+  navigator.geolocation.getCurrentPosition((position) => {
+    
+    socket.emit('sendLocation', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    }, () => {
+      console.log('Location shared!')
+    })
+  })
+})
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const path = require("path");
 const http = require("http");
 const express = require('express');
-const socketio = require("socket.io")
+const socketio = require("socket.io");
+const Filter = require("bad-words");
 
 const app = express();
 const server = http.createServer(app)
@@ -19,15 +20,29 @@ io.on('connection', (socket) => {
 
 console.log('New WebSocket connected');
 
-socket.emit('welcomeMessage', 'Welcome to the app!');
+socket.emit('message', 'Welcome to the app!');
+socket.broadcast.emit('message', 'A new user has joined!');
 
-socket.on('sendMessage', (message) => {
-  io.emit( 'message', message);
-}) 
+socket.on('sendMessage', (message, callback) => {
+  const filter = new Filter()
+
+  if (filter.isProfane(message)){
+    return callback('Profanity is not allowed!')
+  };
 
   
+  io.emit('message', message);
+  callback('Delivered');
+}) 
 
+socket.on('sendLocation', (coords, callback) =>{
+  io.emit("message", `https://google.com/maps?q=${coords.latitude},${coords.longitude}`);
+  callback()
+})
 
+socket.on('disconnect', () => {
+ io.emit('message', 'A user has left!');
+})
 
 // socket.emit('countUpdated', count);
 
