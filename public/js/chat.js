619f8dac59b4c5ccf2971cdab8cfb9f2614d3f29
Casey <89794911+cmurphy20-CVTC@users.noreply.github.com>
2022-06-21 21:55:40 -0500

    message form clears and refocuses and send location only fires once

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,18 +5,31 @@ const socket = io();
 
 // client (emit) -> server (receive) --acknowledgement --> client
 
+// Elements 
+const messageForm = document.querySelector('#messageForm');
+const messageFormInput = messageForm.querySelector('input');
+const messageFormButton = messageForm.querySelector('button');
+const sendLocationButton = document.querySelector('#sendLocation');
+
 socket.on('message', (message) => {
   console.log(message);
 })
 
 
-document.querySelector('#messageForm').addEventListener("submit", (e) => {
+messageForm.addEventListener("submit", (e) => {
 
   e.preventDefault();
 
+  messageForm.setAttribute('disbaled', 'disabled');
+  
   const message = e.target.elements.message.value;
 
   socket.emit('sendMessage', message, (error) => {
+
+    messageForm.removeAttribute('disabled');
+    messageFormInput.value = '';
+    messageFormInput.focus();
+
     if (error) {
       return console.log(error)
     } else {
@@ -25,13 +38,17 @@ document.querySelector('#messageForm').addEventListener("submit", (e) => {
   });
 })
 
-document.querySelector('#sendLocation').addEventListener('click', () => {
+sendLocationButton.addEventListener('click', () => {
   if (!navigator.geolocation) {
     return alert('Geolocation is not supported by your browser.')
   }
 
+  sendLocationButton.setAttribute('disabled', 'disabled');
+
   navigator.geolocation.getCurrentPosition((position) => {
     
+    sendLocationButton.removeAttribute('disabled');
+
     socket.emit('sendLocation', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
