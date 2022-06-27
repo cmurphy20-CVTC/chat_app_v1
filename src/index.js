2a0cae29f1edeafc94d4a4a3b783c52bcfbfae0c
Casey <89794911+cmurphy20-CVTC@users.noreply.github.com>
2022-06-26 21:03:13 -0500

    timestamps added to messages

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const http = require("http");
 const express = require('express');
 const socketio = require("socket.io");
 const Filter = require("bad-words");
+const { generateMessage} = require("./utils/messages")
+const { generateLocationMessage} = require("./utils/messages")
 
 const app = express();
 const server = http.createServer(app)
@@ -20,8 +22,9 @@ io.on('connection', (socket) => {
 
 console.log('New WebSocket connected');
 
-socket.emit('message', 'Welcome to the app!');
-socket.broadcast.emit('message', 'A new user has joined!');
+socket.emit('message', generateMessage("Welcome!"));
+
+socket.broadcast.emit('message', generateMessage('A new user has joined!'));
 
 socket.on('sendMessage', (message, callback) => {
   const filter = new Filter()
@@ -30,18 +33,17 @@ socket.on('sendMessage', (message, callback) => {
     return callback('Profanity is not allowed!')
   };
 
-  
-  io.emit('message', message);
+  io.emit('message', generateMessage(message));
   callback('Delivered');
 }) 
 
 socket.on('sendLocation', (coords, callback) =>{
-  io.emit("locationMessage", `https://google.com/maps?q=${coords.latitude},${coords.longitude}`);
+  io.emit("locationMessage", generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`));
   callback()
 })
 
 socket.on('disconnect', () => {
- io.emit('message', 'A user has left!');
+ io.emit(generateMessage('message'), 'A user has left!');
 })
 
 // socket.emit('countUpdated', count);
